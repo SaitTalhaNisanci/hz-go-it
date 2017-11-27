e3560f7ae476ace13538c89d37f72dc8927d22c5
baris <baris.aydinoz@gmail.com>
2017-11-27 20:38:09 +0300

    additional test case implementations

--- src/github.com/lazerion/acceptance/acceptance.go
+++ src/github.com/lazerion/acceptance/acceptance.go
@@ -9,9 +9,11 @@ import (
 	"github.com/hazelcast/go-client"
 	"github.com/stretchr/testify/assert"
 	"log"
-	"time"
 	"math/rand"
 	"testing"
+	"github.com/hazelcast/go-client/config"
+	"github.com/hazelcast/go-client/core"
+	"time"
 )
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -27,25 +29,32 @@ func randSeq(n int) string {
 type Options struct {
 	ImmediateFail bool
 	ProjectName   string
+	File          string
+}
+
+type Scaling struct {
+	Count int
 }
 
 type AcceptanceFlow struct {
-	options Options
-	project project.APIProject
-	client  hazelcast.IHazelcastInstance
-	context project.Context
+	options    Options
+	project    project.APIProject
+	client     hazelcast.IHazelcastInstance
+	context    project.Context
+	createdMap core.IMap
 }
 
 func NewFlow() AcceptanceFlow {
 	flow := AcceptanceFlow{}
 	flow.options.ImmediateFail = true
 	flow.options.ProjectName = "hazelcast"
+	flow.options.File = "./deployment.yaml"
 	return flow
 }
 
-func (flow AcceptanceFlow) Project(f string) AcceptanceFlow {
+func (flow AcceptanceFlow) Project() AcceptanceFlow {
 	flow.context = project.Context{
-		ComposeFiles: []string{f},
+		ComposeFiles: []string{flow.options.File},
 		ProjectName:  flow.options.ProjectName,
 	}
 
@@ -67,15 +76,15 @@ func (flow AcceptanceFlow) Up() AcceptanceFlow {
 	if err != nil && flow.options.ImmediateFail {
 		log.Fatal(err)
 	}
-	// todo improve wait on event
+	//// todo improve wait on event
 	time.Sleep(5 * time.Second)
 	return flow
 }
 
-func (flow AcceptanceFlow) Scale() AcceptanceFlow {
+func (flow AcceptanceFlow) Scale(options Scaling) AcceptanceFlow {
 
 	m := make(map[string]int)
-	m[flow.options.ProjectName] = 3
+	m[flow.options.ProjectName] = options.Count
 	flow.project.Scale(context.Background(), 10000, m)
 
 	// todo improve wait on event
@@ -91,8 +100,11 @@ func (flow AcceptanceFlow) Down() AcceptanceFlow {
 	return flow
 }
 
-func (flow AcceptanceFlow) Client() AcceptanceFlow {
-	config := hazelcast.NewHazelcastConfig()
+func (flow AcceptanceFlow) DefaultClient() AcceptanceFlow {
+	return flow.Client(hazelcast.NewHazelcastConfig())
+}
+
+func (flow AcceptanceFlow) Client(config *config.ClientConfig) AcceptanceFlow {
 	client, err := hazelcast.NewHazelcastClientWithConfig(config)
 	if err != nil && flow.options.ImmediateFail {
 		flow.Down()
@@ -105,19 +117,70 @@ func (flow AcceptanceFlow) Client() AcceptanceFlow {
 	return flow
 }
 
-func (flow AcceptanceFlow) TryMap(t *testing.T) AcceptanceFlow {
-	mp, err := flow.client.GetMap(randSeq(42))
+func (flow AcceptanceFlow) TryMap(t *testing.T, args ...int) AcceptanceFlow {
+	map_name := randSeq(42)
+	mp, err := flow.client.GetMap(map_name)
 	if err != nil {
 		flow.Down()
 		log.Fatal(err)
 	}
 
+	size, _ := mp.Size()
+	assert.Equal(t, size, int32(0))
+
+	count, valueSize := countAndSize(args...)
+
+	for i := 0; i < count; i++ {
+		key := randSeq(42)
+		value := randSeq(valueSize)
+
+		mp.Put(key, value)
+		actual, _ := mp.Get(key)
+
+		assert.Equal(t, value, actual)
+	}
+
+	s, _ := mp.Size()
+	assert.Equal(t, s, int32(count))
+	mp.Clear()
+	s, _ = mp.Size()
+	assert.Equal(t, s, int32(0))
+
+	flow.createdMap = mp
+	return flow
+}
+
+func countAndSize(args ...int) (int, int) {
+	if len(args) == 0 || args == nil {
+		return 1, 1024
+	}
+	if len(args) == 1 {
+		return args[0], int(1024)
+	}
+	if len(args) > 1 {
+		return args[0], args[1]
+	}
+	return 1,1
+}
+
+func (flow AcceptanceFlow) ExpectError(t *testing.T) AcceptanceFlow {
+	assert.NotNil(t, flow.createdMap)
+
 	key := randSeq(42)
 	value := randSeq(42)
 
-	mp.Put(key, value)
-	actual, _ := mp.Get(key)
+	_, err := flow.createdMap.Put(key, value)
+	if err == nil {
+		log.Fatal("Error expected!")
+	} else {
+		log.Printf("Error is %v", err)
+	}
 
-	assert.Equal(t, value, actual)
+	return flow
+}
+
+func (flow AcceptanceFlow) ExpectConnection(t *testing.T, expected int) AcceptanceFlow {
+	members := flow.client.GetCluster().GetMemberList()
+	assert.Equal(t, len(members), expected)
 	return flow
 }
--- src/github.com/lazerion/acceptance/acceptance_test.go
+++ src/github.com/lazerion/acceptance/acceptance_test.go
@@ -2,15 +2,50 @@ package acceptance
 
 import (
 	"testing"
+	"github.com/hazelcast/go-client"
 )
 
 func TestSingleMemberConnection(t *testing.T) {
 	flow := NewFlow()
-	flow.Project("./deployment.yaml").Up().Client().TryMap(t).Down()
+	flow.Project().Up().DefaultClient().TryMap(t).Down()
 }
 
-func TestClusterDiscovery(t *testing.T)  {
+func TestClusterDiscovery(t *testing.T) {
 	flow := NewFlow()
-	flow.Project("./deployment.yaml").Up().Scale().Client().Down()
+	flow.Project().Up().Scale(Scaling{Count:1}).DefaultClient().TryMap(t).Down()
 }
+
+/**
+Basic Config
+Case 1 - Invocation Timeout/Network Config
+ */
+func TestInvocationTimeout(t *testing.T) {
+	flow := NewFlow()
+	config := hazelcast.NewHazelcastConfig()
+	config.ClientNetworkConfig().SetRedoOperation(true).SetConnectionAttemptLimit(1).SetInvocationTimeoutInSeconds(1)
+
+	flow.Project().Up().Client(config).TryMap(t).Down().ExpectError(t)
+}
+
+/**
+Basic Config
+Case 2 - Smart Routing When Set to False
+ */
+func TestSmartRoutingDisabled(t *testing.T) {
+	flow := NewFlow()
+	config := hazelcast.NewHazelcastConfig()
+	config.ClientNetworkConfig().SetSmartRouting(false)
+
+	flow.Project().Up().Client(config).TryMap(t).ExpectConnection(t, 1).Down()
+}
+
+/**
+Basic Map
+Case 1 - Basic Map Get/Put/Delete
+ */
+func TestBasicMapOperations(t *testing.T) {
+	flow := NewFlow()
+	flow.Project().Up().DefaultClient().TryMap(t, 1024, 1024).Down()
+}
+
 
