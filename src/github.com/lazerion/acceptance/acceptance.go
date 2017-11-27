5c1d14a7afd2de31074d5f39454e84eb36fea04a
baris <baris.aydinoz@gmail.com>
2017-11-27 10:57:32 +0300

    improved with flow on implementation class

--- src/github.com/lazerion/acceptance/acceptance.go
+++ src/github.com/lazerion/acceptance/acceptance.go
@@ -1,1 +1,107 @@
-package hz_go_it
+package acceptance
+
+import (
+	"golang.org/x/net/context"
+	"github.com/docker/libcompose/docker"
+	"github.com/docker/libcompose/docker/ctx"
+	"github.com/docker/libcompose/project"
+	"github.com/docker/libcompose/project/options"
+	"github.com/hazelcast/go-client"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"time"
+	"math/rand"
+	"testing"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+type Options struct {
+	ImmediateFail bool
+	ProjectName   string
+}
+
+type AcceptanceFlow struct {
+	options Options
+	project project.APIProject
+	client  hazelcast.IHazelcastInstance
+}
+
+func NewFlow() AcceptanceFlow {
+	flow := AcceptanceFlow{}
+	flow.options.ImmediateFail = true
+	flow.options.ProjectName = "hazelcast"
+	return flow
+}
+
+func (flow AcceptanceFlow) Project(f string) AcceptanceFlow {
+	project, err := docker.NewProject(&ctx.Context{
+		Context: project.Context{
+			ComposeFiles: []string{f},
+			ProjectName:  flow.options.ProjectName,
+		},
+	}, nil)
+
+	if err != nil && flow.options.ImmediateFail {
+		log.Fatal(err)
+	}
+
+	flow.project = project
+	return flow
+}
+
+func (flow AcceptanceFlow) Up() AcceptanceFlow {
+	name := flow.options.ProjectName
+	err := flow.project.Up(context.Background(), options.Up{}, name)
+	if err != nil && flow.options.ImmediateFail {
+		log.Fatal(err)
+	}
+	// todo improve wait on event
+	time.Sleep(10 * time.Second)
+	return flow
+}
+
+func (flow AcceptanceFlow) Down() AcceptanceFlow {
+	err := flow.project.Down(context.Background(), options.Down{})
+	if err != nil && flow.options.ImmediateFail {
+		log.Fatal(err)
+	}
+	return flow
+}
+
+func (flow AcceptanceFlow) Client() AcceptanceFlow {
+	config := hazelcast.NewHazelcastConfig()
+	client, err := hazelcast.NewHazelcastClientWithConfig(config)
+	if err != nil && flow.options.ImmediateFail {
+		flow.Down()
+		log.Fatal(err)
+	}
+
+	flow.client = client
+	return flow
+}
+
+func (flow AcceptanceFlow) TryMap(t *testing.T) AcceptanceFlow {
+	mp, err := flow.client.GetMap(randSeq(42))
+	if err != nil {
+		flow.Down()
+		log.Fatal(err)
+	}
+
+	key := randSeq(42)
+	value := randSeq(42)
+
+	mp.Put(key, value)
+	actual, _ := mp.Get(key)
+
+	assert.Equal(t, value, actual)
+	return flow
+}
--- src/github.com/lazerion/acceptance/acceptance_test.go
+++ src/github.com/lazerion/acceptance/acceptance_test.go
@@ -1,78 +1,11 @@
-package hz_go_it
+package acceptance
 
 import (
 	"testing"
-	"golang.org/x/net/context"
-	"github.com/docker/libcompose/docker"
-	"github.com/docker/libcompose/docker/ctx"
-	"github.com/docker/libcompose/project"
-	"github.com/docker/libcompose/project/options"
-	"github.com/hazelcast/go-client"
-	"github.com/stretchr/testify/assert"
-	"log"
-	"time"
-	"math/rand"
 )
-
-const (
-	name = "hazelcast"
-)
-
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-func randSeq(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
-}
 
 func TestSingleMemberConnection(t *testing.T) {
-	project, err := docker.NewProject(&ctx.Context{
-		Context: project.Context{
-			ComposeFiles: []string{"./deployment.yaml"},
-			ProjectName:  name,
-		},
-	}, nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = project.Up(context.Background(), options.Up{}, name)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	time.Sleep(10 * time.Second)
-
-	config := hazelcast.NewHazelcastConfig()
-	client, err := hazelcast.NewHazelcastClientWithConfig(config)
-	if err != nil {
-		project.Down(context.Background(), options.Down{}, name)
-		log.Fatal(err)
-	}
-
-
-	mp, err := client.GetMap(randSeq(42))
-	if err != nil {
-		project.Down(context.Background(), options.Down{}, name)
-		log.Fatal(err)
-	}
-
-	key := randSeq(42)
-	value := randSeq(42)
-
-	mp.Put(key, value)
-	actual, _ := mp.Get(key)
-
-	assert.Equal(t, value, actual)
-
-	err = project.Down(context.Background(), options.Down{})
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	flow := NewFlow()
+	flow.Project("./deployment.yaml").Up().Client().TryMap(t).Down()
 }
 
