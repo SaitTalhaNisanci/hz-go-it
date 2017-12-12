f5bf2966440f8fc2ee2d3bc94ec3f4bf90f9efc3
baris <baris.aydinoz@gmail.com>
2017-12-12 11:35:49 +0300

    all tests enabled and updated failure notification email address

--- client.sh
+++ client.sh
@@ -4,7 +4,7 @@ git clone https://github.com/lazerion/hz-go-it
 # uncomment below to test changes on local rather than pushing
 #cd /local/source
 cd ./hz-go-it/acceptance/
-go test -run TestSingleMemberConnection
+go test
 rc=$?
 if [[ ${rc} -ne 0 ]] ; then
   echo 'could not perform tests with success'; exit $rc
--- oss-pipeline.groovy
+++ oss-pipeline.groovy
@@ -65,7 +65,7 @@ pipeline {
             }
         }
         failure {
-            mail to: 'baris@hazelcast.com',
+            mail to: 'clients@hazelcast.com',
                     subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
                     body: "Something is wrong with ${env.BUILD_URL}"
         }
